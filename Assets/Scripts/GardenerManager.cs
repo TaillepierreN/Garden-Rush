81c49ad9f0d29998689fd21708a5267ef31c8d61
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-01-24 11:28:14 +0100

    Added gardener path list

---
+++ Assets/Scripts/GardenerManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class GardenerManager : MonoBehaviour
+{
+    Camera _cam;
+    [SerializeField] NavMeshAgent _gardener;
+    public List<Vector3> targets;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            RaycastHit target;
+            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out target))
+            {
+                targets.Add(target.point);
+
+            }
+        }
+        if (_gardener.remainingDistance <= _gardener.stoppingDistance)
+        {
+            if (targets.Count > 0)
+            {
+                _gardener.destination = targets[0];
+                targets.RemoveAt(0);
+            }
+        }
+
+    }
+
+}
